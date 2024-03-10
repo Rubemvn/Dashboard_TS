c854d6c52d233c40d7c146de25c44bddc422dbd1
Rúbem Vieira <rubemvn17@gmail.com>
2024-03-09 21:31:23 -0300

    Criando componentes

--- dashboard/app/@dashboard/page.tsx
+++ dashboard/app/@dashboard/page.tsx
@@ -1,20 +1,20 @@
+"use client";
+import Aside from "@/components/Aside";
 import Link from "next/link";
 import React from "react";
 
 const page = () => {
-	const nome = "Rúbem";
-	console.log(nome);
 	return (
 		<main>
 			<div>
 				<h1>Dashboard</h1>
 				<p>meu nome é Rúbem</p>
+				<Aside />
 				<Link
 					href={"https://rubemvn.github.io"}
 					target='_blank'>
-					asds
+					portifólio
 				</Link>
-				<input type='text' />
 			</div>
 		</main>
 	);
