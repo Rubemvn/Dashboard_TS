619f90af29f823aa025a79128782769472320157
Rúbem Vieira <rubemvn17@gmail.com>
2024-03-15 16:44:56 -0300

    .

--- dashboard/components/Aside/index.tsx
+++ dashboard/components/Aside/index.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import imagem from "@/public/assets/Captura de tela 2023-11-30 174137.png";
 
-import { Container, Header, LogImg, Title } from "./styles";
+import {
+	Container,
+	Header,
+	LogImg,
+	Title,
+	MenuContainer,
+	MenuItemLink,
+} from "./styles";
+import { useMemo } from "react";
 
 const Aside: React.FC = () => {
+	const logImgUrl = useMemo(
+		() => `data:image/svg+xml;base64,${btoa(imagem)}`,
+		[imagem],
+	);
 	return (
 		<Container>
 			<Header>
-				<LogImg
-					src='@/components/Aside/Dollar-sign.svg'
-					alt={"Logo Minha Carteira"}
-				/>
+				<LogImg src={imagem} />
 				<Title>Minha Carteira</Title>
 			</Header>
+			<MenuContainer>
+				<MenuItemLink href='#'>Dashboard</MenuItemLink>
+			</MenuContainer>
 		</Container>
 	);
 };
--- dashboard/components/Aside/styles.ts
+++ dashboard/components/Aside/styles.ts
@@ -6,6 +6,11 @@ export const Container = styled.div`
 	color: ${(props) => props.theme.colors.black};
 `;
 
-export const Header = styled.div``;
-export const LogImg = styled.img``;
+export const Header = styled.header``;
+export const LogImg = styled.img`
+	width: 50px; /* Ajuste o tamanho conforme necessário */
+	height: auto; /* Isso garantirá que a proporção do SVG seja mantida */
+`;
 export const Title = styled.h1``;
+export const MenuContainer = styled.nav``;
+export const MenuItemLink = styled.a``;
